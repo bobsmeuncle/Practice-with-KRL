a8e3fa831ff4cf9a465ac16527b1e68f737408b6
Adam Burdett <burdettadam@gmail.com>
2014-12-03 11:29:11 -0700

    twilio

--- pythonCode/hello world.py
+++ pythonCode/hello world.py
@@ -3,7 +3,7 @@ import requests
 
 r = requests.post('http://cs.kobj.net/sky/event/FC64E88C-6F51-11E4-8766-859FE71C24E1/123/foursquare/checkin')
 
-r = requests.post('http://cs.kobj.net/sky/event/FC64E88C-6F51-11E4-8766-859FE71C24E1/123/pie/button')
+#r = requests.post('http://cs.kobj.net/sky/event/FC64E88C-6F51-11E4-8766-859FE71C24E1/123/pie/button')
 
 print r.text
 print r.status_code
