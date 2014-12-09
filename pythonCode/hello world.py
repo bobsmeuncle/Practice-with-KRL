284d23e44e0d51dea48901d50776a6d1b128a6c6
Adam Burdett <burdettadam@gmail.com>
2014-12-09 14:29:21 -0700

    more fun

--- pythonCode/hello world.py
+++ pythonCode/hello world.py
@@ -1,10 +1,9 @@
 
 import requests
+import json
 
-r = requests.post('http://cs.kobj.net/sky/event/D8193B5E-4B2F-11E4-8C99-9CD33A8D136F/123/pie/button')
+r = requests.post('http://cs.kobj.net/sky/event/D8193B5E-4B2F-11E4-8C99-9CD33A8D136F/123/raspberrypie/button')
+directives = json.loads(r.text)
 
-#r = requests.post('http://cs.kobj.net/sky/event/D8193B5E-4B2F-11E4-8C99-9CD33A8D136F/123/pie/button')
+print directives['directives'][0]['options']['blinks']
 
-print r.text
-print r.status_code
-print r.headers['content-type']
