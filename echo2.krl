303399c197888b016fad2c5caf172e4d382b5977
Adam Burdett <burdettadam@gmail.com>
2015-02-24 10:35:42 -0700

    lab5 second rule

---
+++ echo2.krl
@@ -0,0 +1,32 @@
+///http://cs.kobj.net/sky/event/FC64E88C-6F51-11E4-8766-859FE71C24E1/123/echo/hello
+///http://cs.kobj.net/sky/event/FC64E88C-6F51-11E4-8766-859FE71C24E1/123/echo/message
+
+ruleset echo2 {
+	meta {  
+		name "echo"
+		description <<
+		echo rule sets.
+		>>
+		author "adam burdett"
+		logging off
+	}
+
+	rule message{
+		select when echo message
+		pre {
+         	foo = event:attr("input");
+      	}
+		{
+			send_directive("sing") with song = foo;
+		}
+
+	}
+
+	rule displayMEWOrking{
+		select when pageview ".*" {
+			notify("version" ,".1");
+
+		}
+
+	}
+}
---
+++ pythonCode/echoterm.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+#r = requests.post('http://download.finance.yahoo.com/d/quotes.csv?s=%40%5EDJI,fb&f=l1&e=.csv')
+r = requests.post('http://cs.kobj.net/sky/event/D8193B5E-4B2F-11E4-8C99-9CD33A8D136F/123/echo/hello')
+directives = json.loads(r.text)
+blinks = directives['directives'][0]['options']['something']
+print blinks
+
+r = requests.post('http://cs.kobj.net/sky/event/D8193B5E-4B2F-11E4-8C99-9CD33A8D136F/123/echo/message?input=Adam_Burdett')
+directives = json.loads(r.text)
+blinks = directives['directives'][0]['options']['something']
+print blinks
