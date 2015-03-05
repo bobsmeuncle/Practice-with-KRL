019fe132fc8fc1cf59e1d2369a26e1621e262aef
Adam Burdett <burdettadam@gmail.com>
2015-03-05 10:21:34 -0700

    ???

--- pythonCode/echoterm.py
+++ pythonCode/echoterm.py
@@ -2,12 +2,12 @@ import requests
 import json
 
 #r = requests.post('http://download.finance.yahoo.com/d/quotes.csv?s=%40%5EDJI,fb&f=l1&e=.csv')
-r = requests.post('http://cs.kobj.net/sky/event/D8193B5E-4B2F-11E4-8C99-9CD33A8D136F/123/echo/hello')
-directives = json.loads(r.text)
-blinks = directives['directives'][0]['options']['something']
-print blinks
+#r = requests.post('http://cs.kobj.net/sky/event/D8193B5E-4B2F-11E4-8C99-9CD33A8D136F/123/echo/hello')
+#directives = json.loads(r.text)
+#blinks = directives['directives'][0]['options']['something']
+#print blinks
 
-r = requests.post('http://cs.kobj.net/sky/event/D8193B5E-4B2F-11E4-8C99-9CD33A8D136F/123/echo/message?input=Adam_Burdett&msg_type=song')
+r = requests.post('http://cs.kobj.net/sky/event/D8193B5E-4B2F-11E4-8C99-9CD33A8D136F/123/echo/message?input=Adam_Burdett')
 directives = json.loads(r.text)
 print directives['directives'][0]['options']
 print directives['directives'][1]['options']
